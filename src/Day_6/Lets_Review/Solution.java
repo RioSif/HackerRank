8df1b784f73612a4837f2bf658f63ac957abb87d
RioSif <riosif@gmail.com>
2021-04-02 02:52:46 +0300

    ⚡️

---
+++ src/Day_6/Lets_Review/Solution.java
@@ -0,0 +1,32 @@
+package Day_6.Lets_Review;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        sc.nextLine();
+
+        for (int i = 0; i < T; i++) {
+            String S = sc.nextLine();
+            String Seven = "";
+            String Sodd = "";
+
+            for (int j = 0; j < S.length(); j += 2) {
+                Seven += S.charAt(j);
+                if (j + 1 < S.length()) {
+                    Sodd += S.charAt(j + 1);
+                }
+            }
+
+            System.out.printf("%s %s\n", Seven, Sodd);
+        }
+    }
+}
