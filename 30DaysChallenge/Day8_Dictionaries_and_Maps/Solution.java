11254783a205bf0182c4e980b52e79c860776725
RioSif <riosif@gmail.com>
2021-04-26 11:20:05 +0300

    :memo:

--- 30DaysChallenge/Day8_Dictionaries_and_Maps/Solution.java
+++ 30DaysChallenge/Day8_Dictionaries_and_Maps/Solution.java
@@ -2,20 +2,23 @@ package Day8_Dictionaries_and_Maps;
 
 //Complete this code or write your own from scratch
 import java.util.*;
-import java.io.*;
 
 class Solution{
     public static void main(String []argh){
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
+        HashMap<String, Integer> phoneCatalog = new HashMap<>();
         for(int i = 0; i < n; i++){
             String name = in.next();
             int phone = in.nextInt();
             // Write code here
+            phoneCatalog.put(name, phone);
         }
         while(in.hasNext()){
             String s = in.next();
             // Write code here
+            System.out.println(phoneCatalog.get(s));
+
         }
         in.close();
     }
