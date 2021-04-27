f9ba98a2ec00ca7558b9f22e7d5dbb172d3422eb
RioSif <riosif@gmail.com>
2021-04-28 02:11:42 +0300

    ✅

--- ProjectEuler/$2_Even_Fibonacci_numbers/Solution.java
+++ ProjectEuler/$2_Even_Fibonacci_numbers/Solution.java
@@ -8,11 +8,29 @@ import java.util.regex.*;
 
 public class Solution {
 
+    public static long sumeven(long n) {
+        // your code
+        long result = 0;
+        long fSum = Long.MAX_VALUE;
+        long fibN0 = 0;
+        long fibN = 1;
+        long fibN1;
+
+        while (n >= (fibN1 = fibN + fibN0 )) {
+            result = fibN1 % 2 == 0 ? result + fibN1 : result;
+            fibN0 = fibN;
+            fibN = fibN1;
+        }
+
+        return result;
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int t = in.nextInt();
         for(int a0 = 0; a0 < t; a0++){
             long n = in.nextLong();
+            System.out.println(sumeven(n));
         }
     }
 }
