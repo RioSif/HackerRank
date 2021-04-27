fcc4b3209904e4963421ca9d5da0a680a1f32ad7
RioSif <riosif@gmail.com>
2021-04-28 02:12:04 +0300

    📝

--- ProjectEuler/$3_Largest_prime_factor/Solution.java
+++ ProjectEuler/$3_Largest_prime_factor/Solution.java
@@ -1,18 +1,25 @@
 package $3_Largest_prime_factor;
 
-import java.io.*;
 import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
 
 public class Solution {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int t = in.nextInt();
-        for(int a0 = 0; a0 < t; a0++){
+        for (int a0 = 0; a0 < t; a0++) {
             long n = in.nextLong();
+            long LPF = 0;
+            for (int i = 2; i <= n; i++) {
+                if (n % i == 0) {
+                    LPF = i;
+                    n /= i;
+                }
+                if (n < 2) {
+                    break;
+                }
+            }
+            System.out.println(LPF);
         }
     }
 }
