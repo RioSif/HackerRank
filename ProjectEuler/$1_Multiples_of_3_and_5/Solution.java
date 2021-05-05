1110f1b251597850e7e516ace6c4d62e7735e5ae
RioSif <riosif@gmail.com>
2021-05-05 22:42:48 +0300

    ✅

--- ProjectEuler/$1_Multiples_of_3_and_5/Solution.java
+++ ProjectEuler/$1_Multiples_of_3_and_5/Solution.java
@@ -14,10 +14,12 @@ public class Solution {
         for(long i = 0;i < t;i++) {
             long n = in.nextLong();
             long sum3 = 0, sum5 = 0, minus15 = 0;
+
             sum3 = (n - 1 - ((n - 1) % 3)) / 3;
             sum5 = (n - 1 - ((n - 1) % 5)) / 5;
             minus15 = (n - 1 - ((n - 1) % 15)) / 15;
             long c= 3 * sum3 * (sum3 + 1) / 2 + 5 * sum5 * (sum5 + 1) / 2 - 15 * minus15 * (minus15 + 1) / 2;
+
             System.out.println(c);
         }
     }
